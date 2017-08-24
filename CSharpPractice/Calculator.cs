4e383fed182f412699ec89fabe5b57bc07147e60
Julia Chen <juliachenjing@gmail.com>
2017-08-24 12:22:35 -0700

    draft

---
+++ CSharpPractice/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpPractice
+{
+
+    class Calculator
+    {
+      
+
+        internal static int subtract(int v1, int v2)
+        {
+            return v1 - v2;
+        }
+    }
+}
---
+++ CSharpPractice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace CSharpPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* my first program in C# */
+            Console.WriteLine("Hello World");
+            Console.WriteLine("a+b= " + add(3, 2));
+            Console.WriteLine("a-b= " + CSharpPractice.Calculator.subtract(3, 2));
+            Console.ReadKey();
+        }
+
+        private static int add(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+    }
+}
